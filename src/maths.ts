3d7fc423930b04d52e3a69d0d8009e336e68cc9d
bernborgess <bernborgess@outlook.com>
2023-11-20 22:24:21 +0000

    ref: renaming

--- src/maths.ts
+++ src/maths.ts
@@ -2,16 +2,16 @@
 // 1. Áreas de figuras geométricas
 
 // Retângulo
-export function area_retangulo(b: number, h: number) {
+export function area_rectangle(b: number, h: number) {
     return b * h;
 }
 
 // Triângulo
-export function area_triangulo(b: number, h: number) {
+export function area_triangle(b: number, h: number) {
     return b * h / 2;
 }
 
 // Círculo
-export function area_circulo(r: number) {
+export function area_circle(r: number) {
     return Math.PI * r * r;
 }
--- test/maths.test.ts
+++ test/maths.test.ts
@@ -1,9 +1,9 @@
-import { area_circulo } from "../src/maths";
+import { area_circle } from "../src/maths";
 
 describe("Maths module", () => {
 
     test("Area of circle of radius 1 is PI", () => {
-        expect(area_circulo(1)).toBe(Math.PI);
+        expect(area_circle(1)).toBe(Math.PI);
     })
 
 })
