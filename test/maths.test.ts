2bd85a3191ce1b4e3b3d448561263c3fc25b43ea
bernborgess <bernborgess@outlook.com>
2023-11-20 22:26:11 +0000

    test: add more tests

--- test/maths.test.ts
+++ test/maths.test.ts
@@ -1,6 +1,18 @@
-import { area_circle } from "../src/maths";
+import {
+    area_circle,
+    area_rectangle,
+    area_triangle
+} from "../src/maths";
 
 describe("Maths module", () => {
+
+    test("Area of 2x5 rectangle is 10", () => {
+        expect(area_rectangle(2, 5)).toBe(10);
+    })
+
+    test("Area of 2x3 triangle is 3", () => {
+        expect(area_triangle(2, 3)).toBe(3);
+    })
 
     test("Area of circle of radius 1 is PI", () => {
         expect(area_circle(1)).toBe(Math.PI);
